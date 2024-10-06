72ec20373d40fdddf45c0ecff929a46be0fc8b26
nithyalokuge <nithyasamadhilokuge@gmail.com>
2024-10-06 19:37:23 +0100

    Added files

---
+++ Lab 1 - 19 Sep 2024/variables.js
@@ -0,0 +1,18 @@
+// Create a set of variables
+let firstName = "Nithya";
+let age = 24;
+let course = "IDM";
+
+console.log("My name is " + firstName + ".");
+console.log("I'm " + age + ". ");
+console.log("The course's name is " + course + ".");
+
+// Change the values of some of your variables
+course = "PDM";
+console.log("The course's name is " + course + ".");
+
+// Create an array with 5 items. List the second and third items.
+let hobbies = ["reading", "swimming", "hiking", "cooking"];
+hobbies[0] = "playing";
+console.log(hobbies[0]);
+console.log("I like " + hobbies[1] + " and " + hobbies[2] + "." );
