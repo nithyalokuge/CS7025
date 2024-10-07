6b39c5241053d13c1b216334639f8867e6555140
nithyalokuge <nithyasamadhilokuge@gmail.com>
2024-10-07 18:58:46 +0100

    Update variables.js

--- Lab 1 - 19 Sep 2024/variables.js
+++ Lab 1 - 19 Sep 2024/variables.js
@@ -1,18 +1,18 @@
 // Create a set of variables
-let firstName = "Nithya";
-let age = 24;
+let favouriteColour = "green";
+let favouriteAnimal = "dolphin";
 let course = "IDM";
 
-console.log("My name is " + firstName + ".");
-console.log("I'm " + age + ". ");
-console.log("The course's name is " + course + ".");
+console.log("My favourite colour is " + favouriteColour + ".");
+console.log("My favourite animal is the " + favouriteAnimal + ".");
+console.log("The course name is " + course + ".");
 
 // Change the values of some of your variables
-course = "PDM";
-console.log("The course's name is " + course + ".");
+favouriteAnimal = "dog";
+console.log("My favourite animal is the " + favouriteAnimal + ".");
 
 // Create an array with 5 items. List the second and third items.
-let hobbies = ["reading", "swimming", "hiking", "cooking"];
-hobbies[0] = "playing";
-console.log(hobbies[0]);
+let hobbies = ["reading", "swimming", "hiking", "cooking", "running"];
+hobbies[3] = "working out";
+console.log(hobbies[3]);
 console.log("I like " + hobbies[1] + " and " + hobbies[2] + "." );
