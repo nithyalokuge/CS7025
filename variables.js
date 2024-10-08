e595f27e4b648acb12209f7c0c910c3405b8efb1
nithyalokuge <nithyasamadhilokuge@gmail.com>
2024-10-08 10:12:41 +0100

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,18 @@
+// Create a set of variables
+let favouriteColour = "green";
+let favouriteAnimal = "dolphin";
+let course = "IDM";
+
+console.log("My favourite colour is " + favouriteColour + ".");
+console.log("My favourite animal is the " + favouriteAnimal + ".");
+console.log("The course name is " + course + ".");
+
+// Change the values of some of the variables
+favouriteAnimal = "dog";
+console.log("My favourite animal is the " + favouriteAnimal + ".");
+
+// Create an array with 5 items. List the second and third items.
+let hobbies = ["reading", "swimming", "hiking", "cooking", "running"];
+hobbies[3] = "working out";
+console.log(hobbies[3]);
+console.log("I like " + hobbies[1] + " and " + hobbies[2] + "." );
