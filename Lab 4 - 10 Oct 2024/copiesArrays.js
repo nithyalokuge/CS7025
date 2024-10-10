8881d7bfbf860caf34be9f932be09b8a9717eab9
nithyalokuge <nithyasamadhilokuge@gmail.com>
2024-10-10 12:10:50 +0100

    Updated files

---
+++ Lab 4 - 10 Oct 2024/copiesArrays.js
@@ -0,0 +1,17 @@
+// Custom exercise
+
+// Make a copy of the first array
+let firstArr = [1,2,3,4];
+let copyOfFirstArr = [...firstArr];
+console.log(firstArr);
+console.log(copyOfFirstArr);
+firstArr.push(5);
+console.log(firstArr);
+console.log(copyOfFirstArr);
+
+// Make a copy of the second array
+let secondArr = [7,8,9,10];
+let copyOfSecondArr = [...secondArr];
+secondArr[0] = "box";
+console.log(secondArr);
+console.log(copyOfSecondArr); 
