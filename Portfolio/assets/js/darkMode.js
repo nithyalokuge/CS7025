7a46ce9ac009efe3a581f880b2fa9b33ef2e7031
nithyalokuge <nithyasamadhilokuge@gmail.com>
2024-11-24 16:35:20 +0000

    Updated files

--- Portfolio/assets/js/darkMode.js
+++ Portfolio/assets/js/darkMode.js
@@ -1,43 +1,56 @@
-// SCRIPT FOR DARK MODE OR LIGHT MODE
+// SCRIPT FOR DARK MODE
 
 document.addEventListener("DOMContentLoaded", () => {
     const darkModeToggle = document.getElementById("dark-mode-toggle");
     const modeIcon = document.getElementById("mode-icon");
 
+    const linkedinIcon = document.querySelector(".linkedin-icon");
+    const githubIcon = document.querySelector(".github-icon");
+
+    // Function to change the src of icons
+    const updateIcons = (isDarkMode) => {
+        if (linkedinIcon) {
+            linkedinIcon.src = isDarkMode
+                ? "assets/img/linkedin_light.png"
+                : "assets/img/linkedin.png";
+        }
+        if (githubIcon) {
+            githubIcon.src = isDarkMode
+                ? "assets/img/github_light.png"
+                : "assets/img/github.png";
+        }
+    };
+
     // Check if the user has a saved theme preference in localStorage
-    if (localStorage.getItem("theme") === "dark") {
-        document.body.classList.add("dark-mode");  
-        // Sun icon for light mode
-        modeIcon.src = "assets/img/sun.png";  
+    const isDarkMode = localStorage.getItem("theme") === "dark";
+    if (isDarkMode) {
+        document.body.classList.add("dark-mode");
+        modeIcon.src = "assets/img/sun.png";
         modeIcon.alt = "Sun icon for light mode";
     } else {
-        document.body.classList.remove("dark-mode");  
-        // Moon icon for dark mode
-        modeIcon.src = "assets/img/moon.png";  
+        document.body.classList.remove("dark-mode");
+        modeIcon.src = "assets/img/moon.png";
         modeIcon.alt = "Moon icon for dark mode";
     }
 
+    // Update icons for the current mode
+    updateIcons(isDarkMode);
+
     // Toggle dark mode when the button is clicked
     darkModeToggle.addEventListener("click", () => {
-        // Toggle dark mode class on body
-        document.body.classList.toggle("dark-mode");
+        const isDarkModeActive = document.body.classList.toggle("dark-mode");
 
-        if (document.body.classList.contains("dark-mode")) {
+        if (isDarkModeActive) {
             modeIcon.src = "assets/img/sun.png";
             modeIcon.alt = "Sun icon for light mode";
-
-            // Save the dark mode preference in localStorage
             localStorage.setItem("theme", "dark");
         } else {
             modeIcon.src = "assets/img/moon.png";
             modeIcon.alt = "Moon icon for dark mode";
-
-            // Save the light mode preference in localStorage
             localStorage.setItem("theme", "light");
         }
+
+        // Update icons for the toggled mode
+        updateIcons(isDarkModeActive);
     });
 });
-
-
-
-
